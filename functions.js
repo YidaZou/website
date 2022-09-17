900a181b04728a1a15d7efd79d8dc4ab23074227
Yida Zou <66279535+YidaZou@users.noreply.github.com>
2022-09-17 17:01:47 -0500

    website

--- functions.js
+++ functions.js
@@ -1,2 +1,23 @@
-grass = 0
-if(button)
+function load_style()
+{
+    page_style = localStorage.getItem("page_stylesheet_name");
+    if (page_style === null)
+    {
+   	    page_style = "duck.css";
+    }
+    document.getElementById('page_style').setAttribute("href", page_style);
+}
+
+function light_mode()
+{
+    localStorage.setItem("page_stylesheet_name", "light.css");
+    load_style();
+}
+
+function dark_mode()
+{
+    localStorage.setItem("page_stylesheet_name", "duck.css");
+    load_style();
+}
+
+load_style();
